678b6965519fbab5b8efd7c1e736824b9129e59b
Kriss Cerón <krissceron03@outlook.com>
2023-05-13 12:33:41 -0500

    PAQUETERIA

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        JFrame buscador= new interfaz_paq("Ejercicio");
+        JFrame buscador= new interfaz_paq("PAQUETERIA");
         buscador.setVisible(true);
     }
 }
